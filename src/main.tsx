83a4e07de0ae0b668bac98ba85824a540200a87a
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2023-11-13 05:12:50 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import reactDom from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
+
+reactDom.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
+);
